d9e06a124e10a82cdd6e3fd8488b4e82549b663c
Nayan Pereira Couto Alves <pereira.nayan@gmail.com>
2022-09-11 20:38:27 -0300

    Alfa 0.2.6 - Toast

--- frontend/src/components/SalesCard/index.tsx
+++ frontend/src/components/SalesCard/index.tsx
@@ -66,8 +66,8 @@ function SalesCard() {
             {sales.map(sale => {
               return (
                 <tr key={sale.id}>
-                  <td className="show576">{new Date(sale.date).toLocaleDateString()}</td>
                   <td className="show992">{sale.id}</td>
+                  <td className="show576">{new Date(sale.date).toLocaleDateString()}</td>
                   <td>{sale.sellerName}</td>
                   <td className="show992">{sale.visited}</td>
                   <td className="show992">{sale.deals}</td>
